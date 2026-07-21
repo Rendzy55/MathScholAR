bed6a6407cb8ad473c2cd43d4668ba1bf04df7d2
Rendzy55 <htmlpython1@gmail.com>
2026-07-21 10:00:56 +0700

    [vUpdate] Cleanup test files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.explorebyte.ar"
         minSdk = 24
         targetSdk = 34
-        versionCode = 23
-        versionName = "1.22"
+        versionCode = 24
+        versionName = "1.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
